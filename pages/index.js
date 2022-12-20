0189ae8297402f737fc2ae126ca6297a2928f238
Panji97 <panjisetyokurniawan@gmail.com>
2022-12-21 01:22:34 +0700

    server side code using file system node js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
+import path from "path";
+import fs from "fs";
+
 export default function Home(props) {
   const { products } = props;
   return (
     <ul>
       {products.map((product) => {
-        return <li key={product.id}>{product.name}</li>;
+        return (
+          <li key={product.id}>
+            <span>{product.name}</span> <p>{product.price}</p>
+          </li>
+        );
       })}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
+  const file = await fs.readFileSync(filePath);
+  const data = JSON.parse(file.toString());
   return {
     props: {
-      products: [
-        { id: "1", name: "Product 1" },
-        { id: "2", name: "Product 2" },
-        { id: "3", name: "Product 3" },
-        { id: "4", name: "Product 4" },
-        { id: "5", name: "Product 5" },
-      ],
+      products: data.products,
     },
   };
 }
